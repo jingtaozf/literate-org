fb63e800cd2ec20b5f2ce01a4155ce7947742ebe
Jingtao Xu <jingtaozf@gmail.com>
2024-09-27 11:10:21 +0800

    Publish a new version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'literate_python',
-    version='0.0.2',
+    version='0.0.3',
     url = 'https://github.com/jingtaozf/literate-python',
     packages=find_packages(),  # Automatically find packages in the directory
     py_modules=['literate_python'])
